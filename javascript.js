efab06da8cdf33cf249183fb59e50481ded89dc1
stysiu2000 <56485919+stysiu2000@users.noreply.github.com>
2020-06-16 12:23:55 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,39 @@
+
+// const logo = document.querySelector('.logo');
+
+// const robert = document.querySelector('.robert');
+
+// const button1 = document.querySelector('.button1');
+
+// const tl = new TimelineMax();
+
+// tl.fromTo(logo, 1,{ height: "0%" }, {height: "80%"});
+
+// .fromTo(introduction, 0.5, {width: '100%'}, {width: '100%', ease: Power2.easeInOut})
+
+
+// .fromTo(robert, 0.5, {opacity: 0, x: 30}, {opacity: 1, x: 0}, "-=0.5")
+
+
+
+
+// !!!!!!!!  MODAL BOX
+
+// if ($('.c-modal').length > 0) {
+//     init();
+//   }
+  
+//   function init() {
+//     $('.c-modal .js-modal-close').on('click', function() {
+//       $('.c-modal').removeClass('show');
+//       $('.c-modal-background').removeClass('show');
+//     })
+  
+//     $('.js-modal-open').on('click', function() {
+//       const { id } = $(this).data();
+//       console.log("dupa");
+//       $('.c-modal__inner').append($(`#${id}`).html());
+//       $(`.c-modal-outer`).find('.c-modal').addClass('show');
+//       $(`.c-modal-outer`).find('.c-modal-background').addClass('show');
+//     })
+//   }
